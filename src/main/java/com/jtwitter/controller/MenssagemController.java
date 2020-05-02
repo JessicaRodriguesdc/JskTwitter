5d84ed7d170c9c8549b4cedb6cf6e70cc74f0e20
Jéssica Rodrigues da Costa <jessicarodridc@gmail.com>
2020-05-02 15:22:46 -0300

    concluindo aula 01

--- src/main/java/com/jtwitter/controller/MenssagemController.java
+++ src/main/java/com/jtwitter/controller/MenssagemController.java
@@ -1,15 +1,39 @@
 package com.jtwitter.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jtwitter.domain.mensagem.Mensagem;
+import com.jtwitter.domain.mensagem.MensagemRepository;
 
 @Controller
 @RequestMapping("/mensagem")
 public class MenssagemController {
 
+	@Autowired
+	private MensagemRepository mensagemRepository;
+	
 	@GetMapping(path = "/nova")
-	public String nova() {
+	public String nova(Model model) {
+		Mensagem mensagem = new Mensagem();
+		model.addAttribute("mensagem", mensagem);
+		
 		return "mensagem-criar";
+	}
+	
+	@PostMapping(path = "/criar")
+	public String criar(Mensagem mensagem) {
+		if(!mensagem.getTexto().isEmpty()) {
+			mensagem.setDataHora(LocalDateTime.now());
+			mensagemRepository.save(mensagem);
+			
+		}
+		return "";
 	}
 }
--- src/main/java/com/jtwitter/domain/mensagem/Mensagem.java
+++ src/main/java/com/jtwitter/domain/mensagem/Mensagem.java
@@ -1,5 +1,47 @@
 package com.jtwitter.domain.mensagem;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Mensagem {
 
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private LocalDateTime dataHora;
+	
+	private String texto;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	} 
+	
+	
+	
 }
